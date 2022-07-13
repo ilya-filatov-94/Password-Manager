140a89af0387100e3d5ddac2f0ee0c3be88950d1
Filatov Ilya <ilya-filatov-1994@mail.ru>
2022-07-13 22:26:47 +0300

    update functions for choose network action and send message to email

--- network_connection.cpp
+++ network_connection.cpp
@@ -1,5 +1,5 @@
 #include "network_connection.h"
-//#include <QDebug>
+
 
 Network_connection::Network_connection(QObject *parent) : QObject(parent)
 {
@@ -9,14 +9,21 @@ Network_connection::Network_connection(QObject *parent) : QObject(parent)
     manager = new QNetworkAccessManager(this);
     /*подключение сигнала о завершении получения данных к обработчику полученного ответа*/
     connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(onResult(QNetworkReply*)));
-    date_time = new QDateTime();                            //Текущие дата/время системы для сообщения
     manager2 = new QNetworkAccessManager(this);
     connect(manager2, SIGNAL(finished(QNetworkReply*)), this, SLOT(response_dataBase(QNetworkReply*)));
 }
 
-void Network_connection::check_http_connection(QString received_command)
+Network_connection::~Network_connection()
+{
+    delete manager;
+    delete manager2;
+}
+
+void Network_connection::check_http_connection(QString received_command, QString parameter1, QString parameter2)
 {
     command=received_command;   //строковая команда на выполнение действия из другого потока
+    cmd_parameter1 = parameter1;
+    cmd_parameter2 = parameter2;
 
     //1-ая проверка: отправляем http-запрос get "http://www.msftncsi.com/ncsi.txt"
     //в случае доступности интернет-соединения должен быть загружен файл со строкой "Microsoft NCSI"
@@ -37,12 +44,12 @@ void Network_connection::onResult(QNetworkReply* reply)
 {
     if (reply->error())
     {
-        emit failure_internet_connection(tr("Проверьте соединение с интернетом.\nОтправка сообщения с кодом проверки невозможна\n"));
+        emit failure_internet_connection(tr("Проверьте соединение с интернетом"));
         connection_http=false;
     }
     else
     {
-        //Если запрос выполнен успешно, то читаем строку из файла и проверяем её на соответсвие с эталонной
+        //Если запрос выполнен успешно, то читаем строку из файла и проверяем её на соответсвие с эталонной: "Microsoft NCSI"
         QString str1=reply->readAll();
         if (str1=="Microsoft NCSI")
         {
@@ -52,7 +59,7 @@ void Network_connection::onResult(QNetworkReply* reply)
         }
         else
         {
-            emit failure_internet_connection(tr("Проверьте соединение с интернетом.\nОтправка сообщения с кодом проверки невозможна\n"));
+            emit failure_internet_connection(tr("Проверьте соединение с интернетом"));
             connection_http=false;
         }
     }
@@ -68,14 +75,11 @@ void Network_connection::lookedUp(const QHostInfo &search_host)
 {
    if (search_host.error() != QHostInfo::NoError)
    {
-       //qDebug() << "запрос к dns завершился ошибкой" << search_host.errorString();
-       emit failure_internet_connection(tr("Проверьте соединение с интернетом.\nВозможно требуется авторизация в браузере\n"));
+       emit failure_internet_connection(tr("Проверьте соединение с интернетом."));
        connection_dns=false;
    }
-   QList <QString> list;
    foreach (const QHostAddress &address, search_host.addresses())
    {
-       list << address.toString();
        if (ip_list.contains(address.toString()))
        {
            connection_dns=true;
@@ -88,8 +92,7 @@ void Network_connection::lookedUp(const QHostInfo &search_host)
    }
    else
    {
-       //qDebug() << "хост не определён" << list;
-       emit failure_internet_connection(tr("Проверьте соединение с интернетом.\nВозможно требуется авторизация в браузере\n"));
+       emit failure_internet_connection(tr("Проверьте соединение с интернетом."));
    }
 }
 
@@ -103,82 +106,43 @@ void Network_connection::network_action()
     }
 }
 
-//выбор действия: отправка сообщения с кодом, отправка тестового письма на почту, запрос пароля в базе данных
+//выбор действия: отправка сообщения с кодом на почту/отправка тестового письма на почту/запрос пароля в базе данных
 void Network_connection::choose_action()
 {
-    bool not_found=true;        //команда не найдена или не распознана
-    int index_command=0;        //индекс команды
-    int index_position=0;       //позиция символа в строке команды
-
-    for (int i=0; i<3; i++)
+    if (command == "test_message")
     {
-        if (command.indexOf(array_comand[i], Qt::CaseInsensitive)!=-1)
-        {
-            not_found=false;
-            index_command=i;
-            break;
-        }
+        generating_test_message();
+        getDataConnect();
     }
-
-    if (not_found==false && !command.isEmpty())
+    if (command == "verification")
     {
-        switch(index_command)
-        {
-            case 0:     //mail:@mail.ru - Отправить письмо на почту для проверки адреса и привязки почты
-            mail_destionation=command.remove(0, array_comand[index_command].size());
-            generating_test_message();
-            send_message();
-            break;
-
-            case 1:     //verification:@mail.ru code_mes:12345 - Отправить письмо на почту с кодом подтверждения
-            command.remove(0, array_comand[index_command].size());
-            index_position=command.indexOf(array_comand[3], Qt::CaseInsensitive);
-            if (index_position!=-1)
-            {
-                mail_destionation=command;
-                mail_destionation.remove(index_position-1,(command.size()));
-
-                code_from_message=command;
-                code_from_message.remove(0, (index_position+array_comand[3].size()));
-                Sleep(3000);    //ожидание результата проверки интернет-соединения
-                generating_message_for_auth();
-                send_message();
-            }
-            break;
-
-            case 2:     //request_dataBase:my_password - отправить запрос в базу данных паролей
-            password_for_check=command.remove(0, array_comand[index_command].size());
-            execute_request_to_db();
-            break;
-        }
+        Sleep(3000);
+        generating_message_for_auth(cmd_parameter2);
+        getDataConnect();
     }
-    /*else if (not_found==true && !command.isEmpty())
+    if (command == "request_db")
     {
-        QMessageBox* msg2 = new QMessageBox(QMessageBox::Critical,
-                                                     tr("Ошибка!"),
-                                                     tr("Команда не распознана!"));
-                           if (msg2->exec()==QMessageBox::Ok)
-                           {delete msg2;}
-    }*/
+        execute_request_to_db(cmd_parameter1);
+    }
 }
 
-void Network_connection::execute_request_to_db()
+void Network_connection::execute_request_to_db(QString& checkPassword)
 {
     preffix_pas.clear();
     suffix_pas.clear();
     QString string_url="https://api.pwnedpasswords.com/range/";     //запрос к API
-    QString string_pas=password_for_check;
-    QString hash = (QCryptographicHash::hash(string_pas.toUtf8(), QCryptographicHash::Sha1).toHex());
+    QString hash = (QCryptographicHash::hash(checkPassword.toUtf8(), QCryptographicHash::Sha1).toHex());
 
-    //Берём первые 5 символов хэша - prefix
-    for (int i=0; i<5; i++)
+    for (int i=0; i<(hash.size()); i++)
     {
-        preffix_pas[i]=hash[i];
-    }
-    //Получение хэша без пяти первых символов - suffix
-    for (int i=5; i<(hash.size()); i++)
-    {
-        suffix_pas[i-5]=hash[i];
+        if (i<5)    //первые 5 символов хэша - prefix
+        {
+            preffix_pas[i]=hash[i];
+        }
+        if (i>=5)   //хэш без 5 первых символов - suffix
+        {
+            suffix_pas[i-5]=hash[i];
+        }
     }
     string_url+=preffix_pas;
     QNetworkRequest request2;
@@ -224,23 +188,32 @@ void Network_connection::response_dataBase(QNetworkReply *reply)
     }
 }
 
-
-void Network_connection::send_message()
+void Network_connection::getDataConnect()
 {
-    //Адрес электронной почты получателя - mail_destionation
-    QString name_sender="";                               //Имя получателя
+    emit signal_smtp_data();
+}
 
-    //Адрес SMTP_сервера и его порт, тип шифрования соединения ssl
-    SmtpClient smtp("smtp.gmail.com", 465, SmtpClient::SslConnection);
+void Network_connection::getDataSmtp(QString login, QString password)
+{
+    send_message(login, password, cmd_parameter1, text_message);
+}
+
+void Network_connection::send_message(QString& login, QString& passw, QString& mail_destionation, QString& text_message)
+{
+    //Имя адресата
+    QString name_sender="";
+
+    //Адрес SMTP_сервера и его порт, тип шифрования соединения TLS
+    SmtpClient smtp("smtp.gmail.com", 587, SmtpClient::TlsConnection);
 
     //Аутентификация в smtp сервере от gmail
-    smtp.setUser("@gmail.com");         //почтовый адрес smtp-сервера
-    smtp.setPassword("password");       //пароль
+    smtp.setUser(login);
+    smtp.setPassword(passw);
 
     //Создание сообщения
     MimeMessage message;
-    message.setSender(new EmailAddress("@gmail.com", "corp\n"));    //адрес отправителя и имя отправителя
-    //Ввод адреса электронной почты получателя и имени получателя
+    message.setSender(new EmailAddress(login, "Filatov IV corp\n"));
+    //Ввод адреса электронной почты адресата mail_destionation и имени адресата
     message.addRecipient(new EmailAddress(mail_destionation, name_sender));
     //Ввод темы сообщения
     message.setSubject(theme_message);
@@ -252,33 +225,30 @@ void Network_connection::send_message()
     // Добавление текста сообщения в почту
     message.addPart(&text);
 
-    // Соединение с хостом, логин в smtp сервере и отправка сообщения
+    // Соединение с хостом, логин в smtp сервере
     smtp.connectToHost();
     smtp.login();
 
-    //Проверка отправки сообщения
+    //Отправка и проверка отправки сообщения
     if (!(smtp.sendMail(message)))
     {
-        //qDebug() << "Сообщение не отправлено!";
-        result_send_message(tr("Ошибка отправления письма!"));
+        emit result_send_message(tr("Ошибка отправления письма!"));
     }
     else
     {
-        //qDebug() << "Сообщение успешно отправлено!";
-        result_send_message(tr("Письмо успешно отправлено!"));
+        emit result_send_message(tr("Письмо успешно отправлено!"));
     }
     smtp.quit();
 }
 
-
-void Network_connection::generating_message_for_auth()
+void Network_connection::generating_message_for_auth(QString& code_from_message)
 {
-    theme_message="authentication";                 //Тема сообщения
     text_message.clear();
+    theme_message="authentication";
     text_message=tr("Выполнена попытка входа с ОС ");
     text_message+=QSysInfo::productType()+" "+QSysInfo::productVersion();       //Название и версия ОС в которой запущено приложение
     text_message+=tr("\r\nДата и время попытки входа: ");
-    text_message+=(date_time->currentDateTime()).toString("dd-MM-yyyy hh:mm:ss");
+    text_message+=(date_time.currentDateTime()).toString("dd-MM-yyyy hh:mm:ss");
     text_message+=tr("\r\nОдноразовый код для входа в приложение: ");
     text_message+=code_from_message;
     text_message+=tr("\r\nНикому не сообщайте этот код\r\n");
@@ -287,16 +257,10 @@ void Network_connection::generating_message_for_auth()
 
 void Network_connection::generating_test_message()
 {
-    theme_message="confirm email";                 //Тема сообщения
     text_message.clear();
+    theme_message="confirm email";
     text_message=tr("Данный адрес электронной почты был успешно привязан к приложению Password Manager\r\n");
     text_message+=tr("и будет использоваться для дополнительной защиты при авторизации в приложении.\r\n");
     text_message+=tr("Данное письмо сгенерировано автоматически, пожалуйста не отвечайте на него");
-
 }
 
-
-Network_connection::~Network_connection()
-{
-    //qDebug() << "thread was destroyed";
-}
--- network_connection.h
+++ network_connection.h
@@ -16,52 +16,56 @@
 #include <QSysInfo>
 #include <QDateTime>
 
+
 class Network_connection : public QObject
 {
     Q_OBJECT
 public:
     explicit Network_connection(QObject *parent = nullptr);
     ~Network_connection();
-    void check_http_connection(QString);
+    void check_http_connection(QString, QString, QString);
+    void getDataSmtp(QString, QString);
 
 private:
-    //QList <QString> ip_list={"108.177.14.147", "108.177.14.103", "108.177.14.106", "108.177.14.105", "108.177.14.104", "108.177.14.99"};
+
     QList<QString> ip_list={"131.107.255.255"};
     bool connection_http;
     bool connection_dns;
     bool internet_connection;
     QString command;            //команда на выполнение действия отправленная из другого потока
-    QString array_comand[4]={"mail:","verification:","request_dataBase:","code_mes:"};  //массив команд
-    QString mail_destionation;  //адрес email получателя
-    QString code_from_message;  //сгенерированный код проверки
-    QString theme_message;      //тема сообщения
-    QString text_message;       //текст сообщения
-    QString password_for_check; //пароль отправляемый на проверку в базу данных
-    QString preffix_pas;        //sha1 хэш префикс пароля
-    QString suffix_pas;         //sha1 хэш суффикс пароля
-    QDateTime* date_time;       //текущие дата-время системы
+    QString cmd_parameter1;
+    QString cmd_parameter2;
+    QString theme_message;
+    QString text_message;
+    QString preffix_pas;       //sha1 хэш префикс пароля
+    QString suffix_pas;        //sha1 хэш суффикс пароля
+    QDateTime date_time;       //текущие дата-время системы
 
     QNetworkAccessManager* manager;
     QNetworkAccessManager* manager2;
-    void getData();                 //http get запрос
-    void request_dns();             //запрос в dns
-    void generating_message_for_auth();      //генерация текста сообщения с одноразовым паролем для входа в приложение
-    void generating_test_message();       //генерация тестового письма для подтверждения email
-    void choose_action();           //выбор действия в зависимости от посланной команды
-    void execute_request_to_db();    //выполнить http get-запрос в базу данных паролей
+    void getData();                          //http get запрос
+    void request_dns();                      //запрос в dns
+    void generating_message_for_auth(QString&);      //генерация текста сообщения с одноразовым паролем для входа в приложение
+    void generating_test_message();                  //генерация тестового письма для подтверждения email
+    void choose_action();                               //выбор действия в зависимости от переданной команды
+    void network_action();                              //выполнение команд работы с сетью при успешном интернет соединении
+    void getDataConnect();
+    void send_message(QString&, QString&, QString&, QString&);
+    void execute_request_to_db(QString&);               //выполнить http get-запрос в базу данных паролей
 
 signals:
 
     void failure_internet_connection(QString);  //Результат проверки соединения с интернетом
     void response_mes_db(QString);              //ответ от базы данных паролей
     void result_send_message(QString);          //Результат отправки письма
+    void signal_smtp_data();
 
 private slots:
+
     void onResult(QNetworkReply* reply);
     void lookedUp(const QHostInfo &search_host);
-    void network_action();                          //выполнение команд работы с сетью при успешном интернет соединении
-    void send_message();
     void response_dataBase(QNetworkReply *reply);
+
 };
 
 #endif // NETWORK_CONNECTION_H
